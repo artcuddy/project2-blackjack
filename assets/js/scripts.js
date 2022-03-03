e4f5e706c815da41e3ae4ff22f9c266ab39523e9
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-03-03 10:31:42 +0000

    Add starter functions to script.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,44 @@
+// Game Functions
+
+/**
+ * Call the resetPlayingArea function
+ */
+function getNewGame() {
+
+
+}
+
+/**
+ * 
+ * This function receives an array of cards and returns the total score
+ */
+function computeScore(cards) {
+
+
+}
+
+/**
+ * 
+ * Make a call to deckofcardsapi using the deckID state variale in order
+ * to retrieve draw 4 cards from the deck
+ */
+function newHand() {
+
+
+}
+
+/**
+ * Reset all state variables to their defaults
+ */
+function resetPlayingArea() {
+
+}
+
+/**
+ * 
+ * If any of roundLost or roundWon or roundTied is true, return immediately
+ */
+function hit(target) { 
+
+}
+
