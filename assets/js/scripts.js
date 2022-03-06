59d09b6c3de8274913ce0e243739c8d1a65402f3
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-03-06 14:10:46 +0000

    Add hit functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,49 +1,66 @@
 let blackJackGame = {
-  'player' : {
-    'cardNumberSpan' : '#player-score',
-    'gamesWon' : '#player-games-won',
+  'player': {
+    'playerScore': '#player-score',
+    'playerCards': '#player-cards',
+    'playerGamesWon': '#player-games-won',
   },
-  'computer' : {
-    'cardNumberSpan' : '#computer-score',
-    'gamesWon' : '#computer-games-won',
-  }
+  'computer': {
+    'computerScore': '#computer-score',
+    'computerCards': '#computer-cards',
+    'computerGamesWon': '#computer-games-won',
+  },
+  'wins': 0,
+  'losses': 0,
+  'isStand': false,
+  'isEndOfTurn': false,
+  'pressOnce': false,
 }
 
 //deckofcards API DECKID
 const DECKID = '';
 
+//players 
+const PLAYER = blackJackGame['player'];
+const COMPUTER = blackJackGame['computer'];
+
 //card area selectors
-const COMPUTERCARDS = document.getElementById('computer-cards');
-const PLAYERCARDS = document.getElementById('player-cards');
+// const COMPUTERCARDS = document.getElementById('computer-cards');
+// const PLAYERCARDS = document.getElementById('player-cards');
 
 //scores selectors
-const COMPUTERSCORE = document.getElementById('computer-score');
-const PLAYERSCORE = document.getElementById('player-score');
+// const COMPUTERSCORE = document.getElementById('computer-score');
+// const PLAYERSCORE = document.getElementById('player-score');
 
 //message area selectors
 const MESSAGE = document.getElementById('message');
 
 //audio
 const HITSOUND = new Audio('audio/swish.mp3');
+const WINSOUND = new Audio('audio/cash.mp3');
+const LOSESOUND = new Audio('audio/aww.mp3');
+
+//button event listeners
+document.getElementById('hit').addEventListener('click', hitMe);
+
 
 //button click selectors
-const HIT = document.getElementById('hit');
-const STAY = document.getElementById('stay');
-const NEWGAME = document.getElementById('new-game');
-const NEWCARDS = document.getElementById('new-hand');
+// const HIT = document.getElementById('hit');
+// const STAY = document.getElementById('stay');
+// const NEWGAME = document.getElementById('new-game');
+// const NEWCARDS = document.getElementById('new-hand');
 
-//on click events
-NEWGAME.onclick = getNewGame;
-NEWCARDS.onclick - getNewCards;
-HIT.onclick = ()=>hitMe('player');
-STAY.onclick = ()=>setTimeout(()=>computerPlays(), 700);
+// //on click events
+// NEWGAME.onclick = getNewGame;
+// NEWCARDS.onclick - getNewCards;
+// HIT.onclick = ()=>hitMe('player');
+// STAY.onclick = ()=>setTimeout(()=>computerPlays(), 700);
 
 /**
  * This function should setup a new game, call the API for a new deck and clear the old game 
  */
 function getNewGame() {
 
-    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
+  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
     .then(response => response.json())
     .then(response => {
 
@@ -57,43 +74,59 @@ function getNewGame() {
 function getNewCards() {
 
   fetch(`https://deckofcardsapi.com/api/deck/${DECKID}/draw/?count=4`)
-  .then(response => response.json())
-  .then(response => {
-  
+    .then(response => response.json())
+    .then(response => {
 
-  })
-  .catch(console.error)
+
+    })
+    .catch(console.error)
 }
 
 /**
  * This function should get a new card from the API when the hit button is clicked 
  */
 function hitMe() {
-  if (roundLost || roundWon || roundTied) {return}
-  fetch(`https://deckofcardsapi.com/api/deck/${DECKID}/draw/?count=1`)
-  .then(response => response.json())
-  .then(response => {
+  if (blackJackGame['isStand'] === false) {
+    fetch(`https://deckofcardsapi.com/api/deck/${DECKID}/draw/?count=1`)
+      .then(response => response.json())
+      .then(response => {
+        if (PLAYER === 'player') {
+          blackJackGame['playerCards'].push(response.cards[0])
+          let cardDomElement = document.createElement("img");
+          cardDomElement.src = response.cards[0].image;
+          blackJackGame['playerCards'].appendChild(cardDomElement)
 
-  })
+          // playerScore = computeScore(playerCards);
+
+          blackJackGame['playerScore'].textContent = playerScore;
+          if (playerScore > 21) {
+            roundLost = true;
+            MESSAGE.textContent = "You broke. You Lose Your Soul!"
+          }
+
+        }
+
+      })
+  }
 }
 
-/**
- * This function should clear the old game and reset all variables to the default settings
- */
-function resetCardGame() {
+  /**
+   * This function should clear the old game and reset all variables to the default settings
+   */
+  function resetCardGame() {
 
-}
+  }
 
-/**
- * This function controls when the computer plays
- */
-function computerPlays() {
+  /**
+   * This function controls when the computer plays
+   */
+  function computerPlays() {
 
-}
+  }
 
-/**
- * This function calculates and matches the scores
- */
-function calculateScore() {
+  /**
+   * This function calculates and matches the scores
+   */
+  function calculateScore() {
 
-}
+  }
