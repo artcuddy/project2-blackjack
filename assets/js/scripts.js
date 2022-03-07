861a43cf270b2f9926d7cd542b9b74fcde47dcdd
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-03-07 09:50:06 +0000

    Add game selectors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,27 @@
-// to avoid global variable drama
+// Global object to avoid global variable drama
 var cardApp = {};
 
+// game variables 
+cardApp.deckID = "";
+cardApp.dealerCards = [];
+cardApp.playerCards = [];
+cardApp.roundLost = false;
+cardApp.roundWon = false;
+cardApp.roundTied = false;
+cardApp.playerScore = 0;
+cardApp.dealerScore = 0;
+
+// score nodes
+cardApp.computerScoreNode = document.getElementById('computer-score');
+cardApp.playerScoreNode = document.getElementById('player-score');
+
+// card area nodes
+cardApp.computerCardsNode = document.getElementById('computer-cards');
+cardApp.playerCardsNode = document.getElementById('player-cards');
+
+// other nodes
+cardApp.messageNode = document.getElementById('message');
+cardApp.newDeckNode = document.getElementById('new-game');
+cardApp.nextHandNode = document.getElementById('next-hand');
+cardApp.hitMeNode = document.getElementById('hit');
+cardApp.stayNode = document.getElementById('stay');
